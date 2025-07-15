ba8272901a3db1d68869669041954a0a2d1ef0bb
Edi Aquino de Souza <edsouzza@hotmail.com>
2025-07-15 11:47:54 -0300

    finalized header

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -1,8 +1,23 @@
+import { Logs, Search } from "lucide-react"
+import FotoPerfil from '../img/Lino.jpg'
+import './styles/adminHeader.css'
+
 function AdminHeader(){
     return ( 
-        <div>
-            Admin Header        
-        </div>
+        <header className="adminheader">
+            <div className="toggle">
+               <Logs size={30}/>
+            </div>     
+            <div className="pesquisar">
+                <label htmlFor="pesquisar">
+                    <input id="pesquisar" type="text" placeholder="Pesquise por aqui"  />
+                    <Search className="lupa"/>
+                </label>
+            </div>  
+            <div className="usuario">
+                <img src={FotoPerfil} alt="Foto de perfil" />
+            </div>
+        </header>
      )
 }
  
